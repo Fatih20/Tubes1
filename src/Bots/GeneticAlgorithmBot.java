1b5dbf37c42b19cd5484a41e0c7deeb7894136a0
Marthenn <89301265+Marthenn@users.noreply.github.com>
2023-10-16 05:08:31 +0700

    feat (GA): made the general idea for the GA bot

--- src/Bots/GeneticAlgorithmBot.java
+++ src/Bots/GeneticAlgorithmBot.java
@@ -1,9 +1,40 @@
 package Bots;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GeneticAlgorithmBot extends Bot {
+    private int populationSize = 100;
+    private int maxGenerations = 100;
 
     public GeneticAlgorithmBot(String playerType) {
         super(playerType);
+    }
+
+    private List<Chromosome> initializePopulation(int populationSize) {
+        List<Chromosome> population = new ArrayList<>();
+        // TODO: implement population initialization
+        return population;
+    }
+
+    private List<Chromosome> selectParents(){
+        List<Chromosome> parents = new ArrayList<>();
+        // TODO: implement parent selection using roulette wheel selection
+        return parents;
+    }
+
+    private List<Chromosome> crossover(List<Chromosome> parents) {
+        List<Chromosome> children = new ArrayList<>();
+        // TODO: implement crossover
+        return children;
+    }
+
+    private void mutate(List<Chromosome> children) {
+        // TODO: call mutate() on each child
+    }
+
+    private void evaluateFitness(List<Chromosome> population) {
+        // TODO: evaluate fitness for each chromosome in population
     }
 
     public int[] move() {
