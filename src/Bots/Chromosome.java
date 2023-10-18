ca9a3118d4157ff4c30b108bc2278367d2f7012f
Marthenn <89301265+Marthenn@users.noreply.github.com>
2023-10-18 17:39:40 +0700

    fix (Chromosome): missing bracket

--- src/Bots/Chromosome.java
+++ src/Bots/Chromosome.java
@@ -9,11 +9,11 @@ import java.util.List;
 import java.util.Random;
 
 public class Chromosome {
-    private List<Pair<Integer,Integer>> genes;
+    private List<Pair<Integer, Integer>> genes;
     private int fitness;
     private final boolean isX;
     private final GameStateBetter gameState;
-    private static final List<Pair<Integer,Integer>> allMoves = List.<Pair<Integer, Integer>>of(
+    private static final List<Pair<Integer, Integer>> allMoves = List.<Pair<Integer, Integer>>of(
             new Pair<>(0, 0),
             new Pair<>(0, 1),
             new Pair<>(0, 2),
@@ -80,60 +80,60 @@ public class Chromosome {
             new Pair<>(7, 7)
     );
 
-    public Chromosome(List<Pair<Integer,Integer>> genes, boolean isX, GameStateBetter gameState){
+    public Chromosome(List<Pair<Integer, Integer>> genes, boolean isX, GameStateBetter gameState) {
         this.genes = genes;
         this.isX = isX;
         this.gameState = gameState;
         this.fitness = 0;
     }
 
-    public List<Pair<Integer,Integer>> getGenes(){
+    public List<Pair<Integer, Integer>> getGenes() {
         return this.genes;
     }
- 
-    public int getFitness(){
+
+    public int getFitness() {
         return this.fitness;
     }
 
     public void setFitness() {
         /*
-        * 1. Make a copy of the game state
-        * 2. Make moves according to the genes
-        * 3. Calculate the fitness
-        * */
-        try{
+         * 1. Make a copy of the game state
+         * 2. Make moves according to the genes
+         * 3. Calculate the fitness
+         * */
+        try {
             GameStateBetter gameStateCopy = (GameStateBetter) this.gameState.clone();
             boolean turn = this.isX;
             for (Pair<Integer, Integer> move : this.genes) {
-                gameStateCopy.play(move.getKey(), move.getValue(), turn,false);
+                gameStateCopy.play(move.getKey(), move.getValue(), turn, false);
                 turn = !turn;
             }
             int XScore = gameStateCopy.getxScore();
             int OScore = gameStateCopy.getoScore();
-            if (this.isX){
+            if (this.isX) {
                 this.fitness = XScore - OScore;
             } else {
                 this.fitness = OScore - XScore;
             }
-        } catch (CloneNotSupportedException | GameStateException.IllegalMove | GameStateException.RowColumnOverFlow e){
+        } catch (CloneNotSupportedException | GameStateException.IllegalMove | GameStateException.RowColumnOverFlow e) {
             throw new RuntimeException(e);
         }
     }
 
-    public void mutate(List<Pair<Integer,Integer>> bannedMoves){
+    public void mutate(List<Pair<Integer, Integer>> bannedMoves) {
         /*
-        * Idea is usage of randomMutationPoint
-        * 1. Generate a random number between 0 and 1
-        * 2. If the number is less than mutationTreshold, then mutation happens
-        * 3. Generate a random mutation point
-        * 4. Generate a random move at the mutation point
-        * 5. Check if the move is not in the bannedMoves list and unique in the chromosome
-        * 6. If it is, then repeat 4-5
-        * */
+         * Idea is usage of randomMutationPoint
+         * 1. Generate a random number between 0 and 1
+         * 2. If the number is less than mutationTreshold, then mutation happens
+         * 3. Generate a random mutation point
+         * 4. Generate a random move at the mutation point
+         * 5. Check if the move is not in the bannedMoves list and unique in the chromosome
+         * 6. If it is, then repeat 4-5
+         * */
         Random random = new Random();
         double mutationChance = random.nextDouble();
         double mutationTreshold = 0.15;
-        if (mutationChance < mutationTreshold){ // mutation happens
+        if (mutationChance < mutationTreshold) { // mutation happens
             int randomMutationPoint = random.nextInt(this.genes.size());
             Pair<Integer, Integer> move = this.genes.get(randomMutationPoint);
             List<Pair<Integer, Integer>> allowedMoves = new ArrayList<>(allMoves);
@@ -145,29 +145,29 @@ public class Chromosome {
     }
 
     /*
-    * Used to compare two chromosomes by their genes
-    * */
-    public boolean equals(Chromosome chromosome){
-        if (this.genes.size() != chromosome.getGenes().size()){
+     * Used to compare two chromosomes by their genes
+     * */
+    public boolean equals(Chromosome chromosome) {
+        if (this.genes.size() != chromosome.getGenes().size()) {
             return false;
         }
-        for (int i = 0; i < this.genes.size(); i++){
-            if (!this.genes.get(i).equals(chromosome.getGenes().get(i))){
+        for (int i = 0; i < this.genes.size(); i++) {
+            if (!this.genes.get(i).equals(chromosome.getGenes().get(i))) {
                 return false;
             }
         }
         return true;
     }
 
-    public static Chromosome make(int turns, boolean isX, GameStateBetter gameState){
-        Chromosome chromosome = new Chromosome(new ArrayList<>, isX, gameState);
+    public static Chromosome make(int turns, boolean isX, GameStateBetter gameState) {
+        Chromosome chromosome = new Chromosome(new ArrayList<>(), isX, gameState);
         Random random = new Random();
 
-        for (int i = 0 ; i < turns; i++){
+        for (int i = 0; i < turns; i++) {
             List<Pair<Integer, Integer>> allowedMoves = new ArrayList<>(allMoves);
             allowedMoves.removeAll(chromosome.getGenes());
             Pair<Integer, Integer> move = allowedMoves.get(random.nextInt(allowedMoves.size()));
-            while(chromosome.getGenes().contains(move)){
+            while (chromosome.getGenes().contains(move)) {
                 move = allowedMoves.get(random.nextInt(allowedMoves.size()));
             }
             chromosome.getGenes().add(move);
